1b50d3625a42df2106d060b486baf6054f87b26b
Romeo Calota <kicsyromy@gmail.com>
2016-07-10 10:26:26 +0300

    Clean up logger

--- src/inc/librt_logger_p.h
+++ src/inc/librt_logger_p.h
@@ -3,16 +3,22 @@
 
 #include <fmt/format.h>
 
-#ifndef __PRETTY_FUNCTION__
-#define __PRETTY_FUNCTION__ __FUNCTION__
-#endif
+#if defined(_WIN32)
+	#if defined(_MSC_VER)
+		#define __PRETTY_FUNCTION__ __FUNCTION__
+	#endif // _MSC_VER
+
+	#define PATH_SEPARATOR '\\'
+#else
+	#define PATH_SEPARATOR '/'
+#endif // _WIN32
 
 #define LOG(...) \
     do { \
-    std::string log = fmt::format(__VA_ARGS__); \
+    std::string log = std::move(fmt::format(__VA_ARGS__)); \
     fmt::print(stderr, \
             "librt: INFO: {}: {}: {}:\n{}\n", \
-            &__FILE__[strrchr(__FILE__, '/') - __FILE__ + 1], \
+            &__FILE__[strrchr(__FILE__, PATH_SEPARATOR) - __FILE__ + 1], \
             __PRETTY_FUNCTION__, \
             __LINE__, \
             log); \
@@ -22,10 +28,10 @@
 
 #define LOG_WARN(...) \
     do { \
-    std::string log = fmt::format(__VA_ARGS__); \
+    std::string log = std::move(fmt::format(__VA_ARGS__)); \
     fmt::print(stderr, \
             "librt: WARNING: {}: {}: {}:\n{}\n", \
-            &__FILE__[strrchr(__FILE__, '/') - __FILE__ + 1], \
+            &__FILE__[strrchr(__FILE__, PATH_SEPARATOR) - __FILE__ + 1], \
             __PRETTY_FUNCTION__, \
             __LINE__, \
             log); \
@@ -33,10 +39,10 @@
 
 #define LOG_ERROR(...) \
     do { \
-    std::string log = fmt::format(__VA_ARGS__); \
+    std::string log = std::move(fmt::format(__VA_ARGS__)); \
     fmt::print(stderr, \
             "librt: ERROR: {}: {}: {}:\n{}\n", \
-            &__FILE__[strrchr(__FILE__, '/') - __FILE__ + 1], \
+            &__FILE__[strrchr(__FILE__, PATH_SEPARATOR) - __FILE__ + 1], \
             __PRETTY_FUNCTION__, \
             __LINE__, \
             log); \
@@ -44,10 +50,10 @@
 
 #define LOG_FATAL(...) \
     do { \
-    std::string log = fmt::format(__VA_ARGS__); \
+    std::string log = std::move(fmt::format(__VA_ARGS__)); \
     fmt::print(stderr, \
             "librt: FATAL: {}: {}: {}:\n{}\n", \
-            &__FILE__[strrchr(__FILE__, '/') - __FILE__ + 1], \
+            &__FILE__[strrchr(__FILE__, PATH_SEPARATOR) - __FILE__ + 1], \
             __PRETTY_FUNCTION__, \
             __LINE__, \
             log); \
@@ -57,11 +63,11 @@
 #ifdef DEBUG_BUILD
 #define LOG_DEBUG(...) \
     do { \
-    std::string log = fmt::format(__VA_ARGS__); \
+    std::string log = std::move(fmt::format(__VA_ARGS__)); \
     fmt::print(stderr, \
             "librt: DEBUG: {}: {}: {}:\n{}\n", \
-            &__FILE__[strrchr(__FILE__, '/') - __FILE__ + 1], \
             __PRETTY_FUNCTION__, \
+            &__FILE__[strrchr(__FILE__, PATH_SEPARATOR) - __FILE__ + 1], \
             __LINE__, \
             log); \
     } while (0)
