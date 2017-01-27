f25884273714696cb5a56dc1fc391cdf064e2e2f
Romeo Calota <kicsyromy@gmail.com>
2017-01-27 11:21:38 -0800

    Rename librt to libgearbox: rename files and update CMake files

--- cmake/WindowsConfig.cmake
+++ cmake/WindowsConfig.cmake
@@ -1,11 +1,11 @@
 if (${CMAKE_SYSTEM_NAME} MATCHES "Windows")
     add_definitions (-DPLATFORM_WINDOWS)
-    set (RT_PLATFORM_OK TRUE)
+    set (GEARBOX_PLATFORM_OK TRUE)
 
-    set (RT_COMPILER_FLAGS "")
+    set (GEARBOX_COMPILER_FLAGS "")
 
-    set (RT_INCLUDE_DIRS "")
-    set (RT_LIB_DIRS "")
-    set (RT_LIBS "wininet.lib")
+    set (GEARBOX_INCLUDE_DIRS "")
+    set (GEARBOX_LIB_DIRS "")
+    set (GEARBOX_LIBS "wininet.lib")
 endif ()
 
--- cmake/macOSConfig.cmake
+++ cmake/macOSConfig.cmake
@@ -1,11 +1,11 @@
 if (${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
     add_definitions (-DPLATFORM_MACOS)
-    set (RT_PLATFORM_OK TRUE)
+    set (GEARBOX_PLATFORM_OK TRUE)
 
     # Enable Objective C++ and ARC
-    set (RT_COMPILER_FLAGS "${RT_COMPILER_FLAGS} -x objective-c++ -fobjc-arc")
+    set (GEARBOX_COMPILER_FLAGS "${GEARBOX_COMPILER_FLAGS} -x objective-c++ -fobjc-arc")
 
-    set (RT_INCLUDE_DIRS "")
-    set (RT_LIB_DIRS "")
-    set (RT_LIBS "-framework Cocoa")
+    set (GEARBOX_INCLUDE_DIRS "")
+    set (GEARBOX_LIB_DIRS "")
+    set (GEARBOX_LIBS "-framework Cocoa")
 endif ()
