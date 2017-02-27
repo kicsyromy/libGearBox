eb6d5f44f22383909db532a145237d3499c122ea
Romeo Calota <kicsyromy@gmail.com>
2017-02-27 09:06:24 +0200

    Add tests for FolderPrivate::find

--- tests/src/test_folder.cpp
+++ tests/src/test_folder.cpp
@@ -70,6 +70,27 @@ TEST_CASE("Test librt_folder_p and librt_folder", "[folder]")
         }
     }
 
+    SECTION("gearbox::FolderPrivate::find(const std::string &)")
+    {
+        using namespace gearbox;
+
+        Folder f("folder");
+        auto &subfolders = f.priv_->subfolders_;
+        subfolders.insert(std::make_pair("sub1", std::make_unique<Folder>("sub1")));
+        subfolders.insert(std::make_pair("sub2", std::make_unique<Folder>("sub2")));
+        subfolders.insert(std::make_pair("sub3", std::make_unique<Folder>("sub3")));
+
+        auto subf = f.priv_->find("sub1");
+        auto it = f.priv_->subfolders_.find("sub1");
+        REQUIRE((subf == it->second.get()));
+        subf = f.priv_->find("sub2");
+        it = f.priv_->subfolders_.find("sub2");
+        REQUIRE((subf == it->second.get()));
+        subf = f.priv_->find("sub3");
+        it = f.priv_->subfolders_.find("sub3");
+        REQUIRE((subf == it->second.get()));
+    }
+
     SECTION("gearbox::FolderPrivate::addPath(const std::string &, std::size_t, std::uint64_t, std::uint64_t, bool, gearbox::File::Priority")
     {
         using namespace gearbox;
