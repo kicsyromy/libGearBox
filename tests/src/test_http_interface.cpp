d2a6dc04355ca833faa55fe50e3dde5c9c9f4de4
Romeo Calota <kicsyromy@gmail.com>
2017-03-01 08:07:32 +0200

    Add test skeleton for HttpInterface

---
+++ tests/src/test_http_interface.cpp
@@ -0,0 +1,12 @@
+#include <catch.hpp>
+
+#define private public
+#include <libgearbox_http_interface_p.h>
+#include <libgearbox_http_interface.cpp>
+
+TEST_CASE("Test libgearbox_http_interface", "[http]")
+{
+    SECTION("")
+    {
+    }
+}
