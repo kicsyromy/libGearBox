963ee77814992dba946208db51fa9cab686daf5c
Romeo Calota <kicsyromy@gmail.com>
2017-03-04 18:05:30 +0200

    Ignore warnings, in Visual Studio, about unexported symbols. Use header-only version of FMT

--- cmake/WindowsConfig.cmake
+++ cmake/WindowsConfig.cmake
@@ -2,7 +2,7 @@ if (${CMAKE_SYSTEM_NAME} MATCHES "Windows")
     add_definitions (-DPLATFORM_WINDOWS)
     set (GEARBOX_PLATFORM_OK TRUE)
 
-    set (GEARBOX_COMPILER_FLAGS "")
+    set (GEARBOX_COMPILER_FLAGS "/wd4251")
 
     set (GEARBOX_INCLUDE_DIRS "")
     set (GEARBOX_LIB_DIRS "")
