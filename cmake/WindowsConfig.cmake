f291858feaa8b42f5d54935ab3ee2df2514507fe
Romeo Calota <kicsyromy@gmail.com>
2017-03-05 18:36:01 +0200

    Check for minimum version 2015 for Visual Studio

--- cmake/WindowsConfig.cmake
+++ cmake/WindowsConfig.cmake
@@ -2,7 +2,9 @@ if (${CMAKE_SYSTEM_NAME} MATCHES "Windows")
     add_definitions (-DPLATFORM_WINDOWS)
     set (GEARBOX_PLATFORM_OK TRUE)
 
-    set (GEARBOX_COMPILER_FLAGS "/wd4251")
+	if (DEFINED MSVC)
+		set (GEARBOX_COMPILER_FLAGS "/wd4251")
+	endif ()
 
     set (GEARBOX_INCLUDE_DIRS "")
     set (GEARBOX_LIB_DIRS "")
