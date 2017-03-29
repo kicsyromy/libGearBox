caaee50f27703b9fc56fc61d94f0bc44366b55eb
Romeo Calota <kicsyromy@gmail.com>
2017-03-29 09:30:34 +0300

    Add dummy main page and update style

--- cmake/Documentation.cmake
+++ cmake/Documentation.cmake
@@ -1,6 +1,8 @@
 find_package (Doxygen)
 if (DOXYGEN_FOUND)
-    configure_file (${CMAKE_CURRENT_SOURCE_DIR}/Doxyfile.in ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile @ONLY)
+	set (GEARBOX_DOC_FILES "${CMAKE_CURRENT_SOURCE_DIR}/docs/")
+	set (GEARBOX_STYLE_SHEET "${GEARBOX_DOC_FILES}/style.css")
+    configure_file ("${GEARBOX_DOC_FILES}/Doxyfile.in" "${CMAKE_CURRENT_BINARY_DIR}/Doxyfile" @ONLY)
     add_custom_target (
         docs ALL
         ${DOXYGEN_EXECUTABLE} ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile
