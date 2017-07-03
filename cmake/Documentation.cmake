2057c115e16b5dcd64d862d49ee30c94d2a475b7
Romeo Calota <kicsyromy@gmail.com>
2017-07-03 18:03:47 +0300

    Add configuration header based on compilation type in order to prevent wrong import semantics on Windows

--- cmake/Documentation.cmake
+++ cmake/Documentation.cmake
@@ -1,12 +1,12 @@
 find_package (Doxygen)
 if (DOXYGEN_FOUND)
-    set (GEARBOX_DOC_FILES "${CMAKE_CURRENT_SOURCE_DIR}/docs/")
+    set (GEARBOX_DOC_FILES "${PROJECT_SOURCE_DIR}/docs/")
     set (GEARBOX_STYLE_SHEET "${GEARBOX_DOC_FILES}/style.css")
-    configure_file ("${GEARBOX_DOC_FILES}/Doxyfile.in" "${CMAKE_CURRENT_BINARY_DIR}/Doxyfile" @ONLY)
+    configure_file ("${GEARBOX_DOC_FILES}/Doxyfile.in" "${PROJECT_BINARY_DIR}/Doxyfile" @ONLY)
     add_custom_target (
         docs ALL
-        ${DOXYGEN_EXECUTABLE} ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile
-        WORKING_DIRECTORY     ${CMAKE_CURRENT_BINARY_DIR}
+        ${DOXYGEN_EXECUTABLE} ${PROJECT_BINARY_DIR}/Doxyfile
+        WORKING_DIRECTORY     ${PROJECT_BINARY_DIR}
         COMMENT "Generating API documentation" VERBATIM
     )
 else ()
